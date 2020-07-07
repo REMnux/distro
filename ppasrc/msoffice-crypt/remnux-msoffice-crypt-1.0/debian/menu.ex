9aca072709575a83bd36a9e95aa9dd2eea7f872c
Lenny Zeltser <lenny@zeltser.com>
2020-07-06 23:14:29 -0400

    Added msoffice-crypt

--- ppasrc/msoffice-crypt/remnux-msoffice-crypt-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(remnux-msoffice-crypt):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="remnux-msoffice-crypt" command="/usr/bin/remnux-msoffice-crypt"
--- ppasrc/msoffice-crypt/remnux-msoffice-crypt-1.0/debian/remnux-msoffice-crypt.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the remnux-msoffice-crypt package
-#
-0 4	* * *	root	[ -x /usr/bin/remnux-msoffice-crypt_maintenance ] && /usr/bin/remnux-msoffice-crypt_maintenance
