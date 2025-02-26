9789bbc22b57362bc26e7025059bf72ac2f7bcd9
Lenny Zeltser <lennyzeltser@users.noreply.github.com>
2025-02-26 13:45:41 -0500

    Update brxor.py
    Removed the error check that was requiring a key with the full option
    Fixed Python 3 compatibility issues:
    Changed basestring to str
    Updated byte handling in the XOR function
    Changed string patterns to byte patterns where needed
    Fixed file reading to properly read the entire file before processing
    Removed the erroneous return options line that was causing early exit
    Updated byte comparison from '\x00' to 0 for Python 3 compatibility

--- files/brxor.py
+++ files/brxor.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
+#!/opt/brxor/bin/python3
 #
 # brxor is a tool for bruteforcing encoded strings
 # within a boundary defined by a regular expression. It
 # will bruteforce the key value range of 0x1 through 0x255
 # It will use PyEnchant to check strings against known english language words
 #
-# Version 0.01b - still need to test passing regular expressions
-# 
 # brutexor.py Created by Alexander Hanel (Alexander.Hanel@gmail.com)
 # Modified by Trenton Tait (tretait@gmail.com)
 #
@@ -18,122 +16,120 @@
 #   -f, --full            XOR full file
 #   -d DICT, --dict=DICT  User supplied dictionary, one word per line (Default: 'en_us')
 #   -l LEN, --length=LEN  Minimum word length to use (Default: 4)
-# 
-# TODO: case insensitive matches  
+#
 
 import string
-import re 
+import re
 import sys
 import os
-from optparse import OptionParser 
+from argparse import ArgumentParser
 try:
     import enchant
 except:
-    print 'PyEnchant not found. Please install using \'pip install enchant\''
+    print('PyEnchant not found. Please install using \'pip install enchant\'')
     sys.exit(1)
 
 def valid_ascii(char):
     if char in string.printable[:-3]:
         return True
     else:
-        return None 
+        return None
 
 def xor(data, key):
     decode = ''
-    if isinstance(key, basestring):
-        key = int(key,16)
-        
+    if isinstance(key, str):  # Fixed basestring to str for Python 3
+        key = int(key, 16)
+
     for d in data:
-        decode = decode + chr(ord(d) ^ key)
+        if isinstance(d, int):  # Handle bytes in Python 3
+            decode = decode + chr(d ^ key)
+        else:
+            decode = decode + chr(ord(d) ^ key)
     return decode
 
-# http://stackoverflow.com/questions/14678132/python-hexadecimal
 def twoDigitHex(num):
     return '0x%02x' % num
 
-def parse_options():
-    parser = OptionParser()
+def main():
     usage = 'usage: brxor.py [options] <file>'
-    parser  =  OptionParser(usage=usage)
-    parser.add_option('-k', '--key', action='store', dest='key', type='string', help='Static XOR key to use')
-    parser.add_option('-f', '--full', action='store_true', dest='full', help='XOR full file')
-    parser.add_option('-d', '--dict', action='store', dest='user_dict', help="User supplied dictionary, one word per line (Default: 'en_us')")
-    parser.add_option('-l', '--length', action='store', dest='length', default=4, help="Minimum word length to use (Default: 4)")
-    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='Increase verbosity of output.')
-    (options, args) = parser.parse_args()
+    parser = ArgumentParser(usage=usage, description='Python 3 XOR brute-forcer ')
+    parser.add_argument('file', action='store', help='file')
+    parser.add_argument('-k', '--key', action='store', dest='key',help='Static XOR key to use')
+    parser.add_argument('-f', '--full', action='store_true', dest='full', help='XOR full file')
+    parser.add_argument('-d', '--dict', action='store', dest='user_dict', help="User supplied dictionary, one word per line (Default: 'en_us')")
+    parser.add_argument('-l', '--length', action='store', dest='length', default=4, help="Minimum word length to use (Default: 4)")
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Increase verbosity of output.')
+    args = parser.parse_args()
 
     global word_dict
-    
-    # Test for Args 
+
+    # Test for Args
     if len(sys.argv) < 2:
         parser.print_help()
         return
-    # Test that the full option contains a XOR Key
-    if options.full != None and options.key == None:
-        print '[ERROR] --FULL OPTION MUST INCLUDE XOR KEY'
+        
+    # XOR the full file with key if both full and key are provided
+    if args.full and args.key:
+        with open(args.file, 'rb') as f:
+            sys.stdout.write(xor(f.read(), args.key))
         return
-    # XOR the full file with key 
-    if options.full != None and options.key != None:
-        sys.stdout.write(xor(f.read(), options.key))
-        return
-    if options.user_dict:
+        
+    if args.user_dict:
         # check for file
-        if os.path.isfile(options.user_dict) and os.access(options.user_dict, os.R_OK):
-            word_dict = enchant.request_pwl_dict(options.user_dict)
+        if os.path.isfile(args.user_dict) and os.access(args.user_dict, os.R_OK):
+            word_dict = enchant.request_pwl_dict(args.user_dict)
         else:
-            print '[ERROR] FILE CAN NOT BE OPENED OR READ!\n'
-            print usage
+            print('[ERROR] FILE CAN NOT BE OPENED OR READ!\n')
+            print(usage)
             sys.exit(1)
     else:
         word_dict = enchant.Dict('en_US')
 
-    # Parse file for regular expressions 
-    return options
-    
-def main(argv):
-    options = parse_options()
-    regex = re.compile(r'\x00(?!\x00).+?\x00') 
-    buff = ''
+    # Parse file for regular expressions
+    # Removed "return options" line that was causing early exit
 
+    regex = re.compile(rb'\x00(?!\x00).+?\x00')  # Changed to raw bytes pattern
+    
     try:
-        f = open(sys.argv[len(sys.argv)-1],'rb')
+        with open(args.file, 'rb') as f:
+            file_content = f.read()
     except Exception:
-        print '[ERROR] FILE CAN NOT BE OPENED OR READ!'
+        print('[ERROR] FILE CAN NOT BE OPENED OR READ!')
         return
+        
     # for each regex pattern found
-    for match in regex.finditer(f.read()):
+    for match in regex.finditer(file_content):
         if len(match.group()) < 8:
-            continue 
+            continue
         # for XOR key in range of 0x0 to 0xff
-        for key in range(1,0x100):
-            # for each byte in match of regex pattern 
+        for key in range(1, 0x100):
+            buff = ''
+            # for each byte in match of regex pattern
             for byte in match.group():
-                if byte == '\x00':
+                if byte == 0:  # Changed from '\x00' to 0 for Python 3 bytes
                     buff = buff + '\x00'
-                    continue 
+                    continue
                 else:
-                    tmp = xor(byte,key)
+                    tmp = chr(byte ^ key)  # Simplified XOR for Python 3
                     if valid_ascii(tmp) == None:
                         buff = ''
                         break
                     else:
                         buff = buff + tmp
+                        
             if buff != '':
-                words = re.findall(r'\b[a-zA-Z]{%s,}\b' % options.length,buff)
+                words = re.findall(r'\b[a-zA-Z]{%s,}\b' % args.length, buff)
                 # TODO: case insensitive matches
                 enchants = [x for x in words if word_dict.check(x.lower()) == True]
                 if len(enchants) > 0:
-                        # if verbose, show human-readable match along with decode str
-                        if options.verbose:
-                            output = '[%s (%s)] %s %s\n' % (hex(match.start()),twoDigitHex(key),enchants,buff)
-                        else:
-                            output = '[%s (%s)] %s\n' % (hex(match.start()),twoDigitHex(key),buff)
-                        # avoid line breaks in the middle of a string
-                        output = output.strip().replace('\n', '\\n')
-                        sys.stdout.write(output + '\n')
-                buff = ''
-    f.close()
-    return 
+                    # if verbose, show human-readable match along with decode str
+                    if args.verbose:
+                        output = '[%s (%s)] %s %s\n' % (hex(match.start()), twoDigitHex(key), enchants, buff)
+                    else:
+                        output = '[%s (%s)] %s\n' % (hex(match.start()), twoDigitHex(key), buff)
+                    # avoid line breaks in the middle of a string
+                    output = output.strip().replace('\n', '\\n')
+                    sys.stdout.write(output + '\n')
 
 if __name__== '__main__':
-    main(sys.argv[1:])
+    main()
